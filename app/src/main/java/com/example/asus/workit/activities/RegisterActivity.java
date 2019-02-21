503c53f5e7cba1434312730f3f603256936eac7f
Christian Jonathan <christianchrisjo@gmail.com>
2019-02-21 17:22:22 +0700

    added login, register and changes in XML

---
+++ app/src/main/java/com/example/asus/workit/activities/RegisterActivity.java
@@ -0,0 +1,212 @@
+package com.example.asus.workit.activities;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.InputType;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import com.example.asus.workit.R;
+import com.example.asus.workit.helpers.InputValidation;
+import com.example.asus.workit.model.User;
+import com.example.asus.workit.sql.DatabaseHelper;
+
+import java.util.Calendar;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final AppCompatActivity activity = RegisterActivity.this;
+    private String DateOfBirth;
+    private Calendar calendar;
+    private int year, month, day;
+
+    private NestedScrollView nestedScrollView;
+
+    private TextInputLayout textInputLayoutName;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputLayout textInputLayoutConfirmPassword;
+    private TextInputLayout textInputLayoutBodyWeight;
+    private TextInputLayout textInputLayoutDateOfBirth;
+    private TextInputLayout textInputLayoutHeight;
+
+    private TextInputEditText textInputEditTextName;
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+    private TextInputEditText textInputEditTextConfirmPassword;
+    private TextInputEditText textInputEditTextBodyWeight;
+    private TextInputEditText textInputEditTextDateOfBirth;
+    private TextInputEditText textInputEditTextHeight;
+
+    private AppCompatButton appCompatButtonRegister;
+    private AppCompatTextView appCompatTextViewLoginLink;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+    private User user;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar();
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        initViews();
+        appCompatButtonRegister.setOnClickListener(this);
+        appCompatTextViewLoginLink.setOnClickListener(this);
+
+        inputValidation = new InputValidation(activity);
+        databaseHelper = new DatabaseHelper(activity);
+        user = new User();
+    }
+
+
+    private void initViews() {
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+
+        textInputLayoutName = (TextInputLayout) findViewById(R.id.textInputLayoutName);
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
+        textInputLayoutConfirmPassword = (TextInputLayout) findViewById(R.id.textInputLayoutConfirmPassword);
+        textInputLayoutDateOfBirth = (TextInputLayout) findViewById(R.id.textInputLayoutDateOfBirth);
+        textInputLayoutHeight = (TextInputLayout) findViewById(R.id.textInputLayoutHeight);
+        textInputLayoutBodyWeight = (TextInputLayout) findViewById(R.id.textInputLayoutBodyWeight);
+
+
+        textInputEditTextName = (TextInputEditText) findViewById(R.id.textInputEditTextName);
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
+        textInputEditTextConfirmPassword = (TextInputEditText) findViewById(R.id.textInputEditTextConfirmPassword);
+        textInputEditTextDateOfBirth = (TextInputEditText) findViewById(R.id.textInputEditTextDateOfBirth);
+        textInputEditTextHeight = (TextInputEditText) findViewById(R.id.textInputEditTextHeight);
+        textInputEditTextBodyWeight = (TextInputEditText) findViewById(R.id.textInputEditTextBodyWeight);
+
+        //Set Date of Birth editText to not showing keyboard when clicked
+        textInputEditTextDateOfBirth.setInputType(InputType.TYPE_NULL);
+
+        appCompatButtonRegister = (AppCompatButton) findViewById(R.id.appCompatButtonRegister);
+
+        appCompatTextViewLoginLink = (AppCompatTextView) findViewById(R.id.appCompatTextViewLoginLink);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.appCompatButtonRegister:
+                postDataToSQLite();
+                break;
+
+            case R.id.appCompatTextViewLoginLink:
+                finish();
+                break;
+        }
+    }
+
+    private void postDataToSQLite() {
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextName, textInputLayoutName, getString(R.string.error_message_name))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, getString(R.string.error_message_password))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextMatches(textInputEditTextPassword, textInputEditTextConfirmPassword,
+                textInputLayoutConfirmPassword, getString(R.string.error_password_match))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextDateOfBirth, textInputLayoutDateOfBirth, getString(R.string.error_message_dob))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextHeight, textInputLayoutHeight, getString(R.string.error_message_height))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextBodyWeight, textInputLayoutBodyWeight, getString(R.string.error_message_bw))) {
+            return;
+        }
+
+        if (!databaseHelper.checkUser(textInputEditTextEmail.getText().toString().trim())) {
+
+            user.setName(textInputEditTextName.getText().toString().trim());
+            user.setEmail(textInputEditTextEmail.getText().toString().trim());
+            user.setPassword(textInputEditTextPassword.getText().toString().trim());
+
+            user.setDateOfBirth(textInputEditTextDateOfBirth.getText().toString().trim());
+            user.setHeight(Integer.parseInt(textInputEditTextHeight.getText().toString().trim()));
+            user.setBodyWeight(Integer.parseInt(textInputEditTextBodyWeight.getText().toString().trim()));
+
+            databaseHelper.addUser(user);
+
+            Snackbar.make(nestedScrollView, getString(R.string.success_message), Snackbar.LENGTH_LONG).show();
+            emptyInputEditText();
+
+
+        } else {
+            Snackbar.make(nestedScrollView, getString(R.string.error_email_exists), Snackbar.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    private void emptyInputEditText() {
+        textInputEditTextName.setText(null);
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
+        textInputEditTextConfirmPassword.setText(null);
+        textInputEditTextDateOfBirth.setText(null);
+        textInputEditTextHeight.setText(null);
+        textInputEditTextBodyWeight.setText(null);
+    }
+
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                    int picked_year, int picked_month, int picked_day) {
+
+                    DateOfBirth = showDate(picked_year, picked_month+1, picked_day);
+                    textInputEditTextDateOfBirth.setText(DateOfBirth);
+                }
+            };
+
+    private String showDate(int year, int month, int day) {
+        return Integer.toString(day)+"/"+Integer.toString(month)+"/"+Integer.toString(year);
+    }
+}
